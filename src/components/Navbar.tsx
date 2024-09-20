3a4e4073c53bed9036f7be1aa3fe469815e18bac
toniliu <toniliu672@gmail.com>
2024-09-21 07:07:52 +0800

    Add logout in mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { FaDoorOpen, FaBars, FaCaretDown } from 'react-icons/fa';
-import { Link, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import { useAssessments } from '../context/AssessmentContext';
-import LogoutButton from './Logout';
-import DarkModeToggle from './DarkModeToggle';
-import LogoImage from '../assets/icon.png';
+import React, { useState, useEffect, useRef } from "react";
+import { FaDoorOpen, FaBars, FaCaretDown } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useAssessments } from "../context/AssessmentContext";
+import LogoutButton from "./Logout";
+import DarkModeToggle from "./DarkModeToggle";
+import LogoImage from "../assets/icon.png";
 
 const Navbar: React.FC = () => {
   const { assessments } = useAssessments();
@@ -39,14 +39,14 @@ const Navbar: React.FC = () => {
 
   const getLinkClasses = (path: string) => {
     return location.pathname === path
-      ? 'text-orange-700 border-b-2 border-orange-700 font-medium dark:text-orange-500'
-      : 'text-gray-800 hover:text-orange-700 transition duration-300 font-medium dark:text-gray-200 dark:hover:text-orange-500';
+      ? "text-orange-700 border-b-2 border-orange-700 font-medium dark:text-orange-500"
+      : "text-gray-800 hover:text-orange-700 transition duration-300 font-medium dark:text-gray-200 dark:hover:text-orange-500";
   };
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 1080);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
@@ -65,40 +65,37 @@ const Navbar: React.FC = () => {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   const renderDataDropdown = () => (
-    <div
-      className='relative'
-      ref={dataDropdownRef}
-    >
+    <div className="relative" ref={dataDropdownRef}>
       <button
         onClick={toggleDataDropdown}
-        className='text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500'
+        className="text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500"
       >
-        Data <FaCaretDown className='ml-1' />
+        Data <FaCaretDown className="ml-1" />
       </button>
       {dataDropdownOpen && (
-        <div className='absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700'>
+        <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700">
           <Link
-            to='/data-sekolah'
-            className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+            to="/data-sekolah"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
             onClick={() => setDataDropdownOpen(false)}
           >
             Data Sekolah
           </Link>
           <Link
-            to='/data-okupasi'
-            className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+            to="/data-okupasi"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
             onClick={() => setDataDropdownOpen(false)}
           >
             Data Okupasi
           </Link>
           <Link
-            to='/data-konsentrasi'
-            className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+            to="/data-konsentrasi"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
             onClick={() => setDataDropdownOpen(false)}
           >
             Data Konsentrasi
@@ -109,25 +106,22 @@ const Navbar: React.FC = () => {
   );
 
   const renderAssessmentDropdown = () => (
-    <div
-      className='relative'
-      ref={assessmentDropdownRef}
-    >
+    <div className="relative" ref={assessmentDropdownRef}>
       <button
         onClick={toggleAssessmentDropdown}
-        className='text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500'
+        className="text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500"
       >
-        Assessment <FaCaretDown className='ml-1' />
+        Assessment <FaCaretDown className="ml-1" />
       </button>
       {assessmentDropdownOpen && (
-        <div className='absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700'>
+        <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700">
           {assessments.map((assessment) => (
             <a
               key={assessment.id}
               href={assessment.url}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
             >
               {assessment.title}
             </a>
@@ -138,30 +132,27 @@ const Navbar: React.FC = () => {
   );
 
   const renderSettingsDropdown = () => (
-    <div
-      className='relative'
-      ref={settingsDropdownRef}
-    >
+    <div className="relative" ref={settingsDropdownRef}>
       <button
         onClick={toggleSettingsDropdown}
-        className='text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500'
+        className="text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500"
       >
-        Settings <FaCaretDown className='ml-1' />
+        Settings <FaCaretDown className="ml-1" />
       </button>
       {settingsDropdownOpen && (
-        <div className='absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700'>
+        <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-md z-10 dark:bg-gray-700">
           {isSuperAdmin && (
             <Link
-              to='/pengguna'
-              className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+              to="/pengguna"
+              className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
               onClick={() => setSettingsDropdownOpen(false)}
             >
               User Settings
             </Link>
           )}
           <Link
-            to='/assesment'
-            className='block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600'
+            to="/assesment"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-600"
             onClick={() => setSettingsDropdownOpen(false)}
           >
             Assessment Settings
@@ -172,30 +163,20 @@ const Navbar: React.FC = () => {
   );
 
   return (
-    <nav className='fixed w-full z-50 bg-white shadow-md dark:bg-gray-800'>
-      <div className='flex justify-between items-center px-6 py-4'>
-        <div className='flex items-center'>
-          <Link to='/'>
-            <img
-              src={LogoImage}
-              alt='Logo'
-              className='h-10'
-            />
+    <nav className="fixed w-full z-50 bg-white shadow-md dark:bg-gray-800">
+      <div className="flex justify-between items-center px-6 py-4">
+        <div className="flex items-center">
+          <Link to="/">
+            <img src={LogoImage} alt="Logo" className="h-10" />
           </Link>
         </div>
         {isMobile ? (
-          <div
-            className='flex'
-            onClick={toggleMenu}
-          >
-            <FaBars className='text-gray-800 hover:text-orange-700 transition duration-300 dark:text-white' />
+          <div className="flex" onClick={toggleMenu}>
+            <FaBars className="text-gray-800 hover:text-orange-700 transition duration-300 dark:text-white" />
           </div>
         ) : (
-          <div className='flex items-center space-x-6'>
-            <Link
-              to='/'
-              className={getLinkClasses('/')}
-            >
+          <div className="flex items-center space-x-6">
+            <Link to="/" className={getLinkClasses("/")}>
               Home
             </Link>
             {isLoggedIn && (
@@ -210,24 +191,20 @@ const Navbar: React.FC = () => {
               <LogoutButton />
             ) : (
               <Link
-                to='/login'
+                to="/login"
                 className={`text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500 ${getLinkClasses(
-                  '/login',
+                  "/login"
                 )}`}
               >
-                <FaDoorOpen className='mr-2' /> Login
+                <FaDoorOpen className="mr-2" /> Login
               </Link>
             )}
           </div>
         )}
       </div>
       {isMobile && menuOpen && (
-        <div className='bg-white w-full absolute top-16 left-0 right-0 shadow-md z-10 flex flex-col items-center space-y-4 py-4 dark:bg-gray-800'>
-          <Link
-            to='/'
-            className={getLinkClasses('/')}
-            onClick={toggleMenu}
-          >
+        <div className="bg-white w-full absolute top-16 left-0 right-0 shadow-md z-10 flex flex-col items-center space-y-4 py-4 dark:bg-gray-800">
+          <Link to="/" className={getLinkClasses("/")} onClick={toggleMenu}>
             Home
           </Link>
           {isLoggedIn && (
@@ -235,17 +212,18 @@ const Navbar: React.FC = () => {
               {renderDataDropdown()}
               {renderAssessmentDropdown()}
               {renderSettingsDropdown()}
+              <LogoutButton />
             </>
           )}
           {!isLoggedIn && (
             <Link
-              to='/login'
+              to="/login"
               className={`text-gray-800 hover:text-orange-700 transition duration-300 font-medium flex items-center dark:text-gray-200 dark:hover:text-orange-500 ${getLinkClasses(
-                '/login',
+                "/login"
               )}`}
               onClick={toggleMenu}
             >
-              <FaDoorOpen className='mr-2' /> Login
+              <FaDoorOpen className="mr-2" /> Login
             </Link>
           )}
         </div>
